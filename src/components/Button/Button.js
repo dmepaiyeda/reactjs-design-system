84aa9de2c63d1fe072d0864449f2cf09ee60756c
Deborah Mepaiyeda <dmepaiyeda@users.noreply.github.com>
2019-05-29 16:21:29 -0400

    created stories and themes

---
+++ src/components/Button/Button.js
@@ -0,0 +1,34 @@
+import React from 'react';
+// The HOC we wrap our components in to apply styles
+import injectSheet from 'react-jss';
+
+//CSS file in JS object 
+const styles = theme => ({
+      // All top level object keys are different class names
+    myButton: {
+        // Global style 
+        color: theme.text.color,
+        margin: {
+            //jss-expand gives more readable syntax
+            top: 5, //jss-default-unit makes this 5px
+            right: 0, 
+            bottom: 0,
+            left: "1rem"
+        },
+
+        "& span":{
+            fontWeight: "bold"
+        }
+    },
+    myLabel: {
+        fontStyle: "italic"
+    }
+});
+
+const Button = ({classes, children}) => (
+    <button className={classes.myButton}>
+        <span className={classes.myLabel}>{children}</span>
+    </button>
+);
+
+export default injectSheet(styles)(Button)
