0de4c5de00bb5562d613ba7c5644f700da7158b9
Deborah Mepaiyeda <dmepaiyeda@users.noreply.github.com>
2019-06-13 15:32:07 -0400

    updated properties in button component

--- src/stories/button.js
+++ src/stories/button.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "../../src/primitives";
 import { action } from "@storybook/addon-actions";
 
-export const Buttons = ({ text, ...props }) => (
+export const Buttons = ({ ...props }) => (
   <Button {...props} onClick={action("clicked")} py={3} px={2}>
     OUTLINE BUTTON
   </Button>
