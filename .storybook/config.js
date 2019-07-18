20232331a97585f2b2e4abeffdf8f23271338318
Deborah Mepaiyeda <dmepaiyeda@users.noreply.github.com>
2019-07-18 16:54:22 -0400

    set up storybook viewports add on

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,31 @@
-import { configure } from '@storybook/react';
+import { configure } from "@storybook/react";
+import { addParameters } from "@storybook/react";
+
+const newViewports = {
+  kindleFire2: {
+    name: "Kindle Fire 2",
+    styles: {
+      width: "600px",
+      height: "963px"
+    }
+  },
+  kindleFireHD: {
+    name: "Kindle Fire HD",
+    styles: {
+      width: "533px",
+      height: "801px"
+    }
+  }
+};
+
+addParameters({
+  viewport: { viewports: newViewports }
+});
+
+addParameters({ viewport: { viewports: newViewports } });
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src/stories', true, /\.stories\.js$/);
+const req = require.context("../src/stories", true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
--- src/stories/card.stories.js
+++ src/stories/card.stories.js
@@ -5,19 +5,22 @@ import { ProductCard } from "./ProductCard";
 import theme from "../theme/theme";
 
 //put styling related stuff in the html and other stuff in the primitives file
-storiesOf("Patterns|ProductCard", module).add(
-  "Card",
-  () => (
-    <ThemeProvider theme={theme}>
-      <ProductCard
-        title="Heading"
-        subtitle="subheading"
-        image="https://static-assets.glossier.com/production/spree/images/attachments/000/003/860/portrait_normal/01_ShopGrid.jpg"
-        secondImage="https://static-assets.glossier.com/production/spree/images/attachments/000/003/730/portrait_normal/bubblewrap.jpg"
-      />
-    </ThemeProvider>
-  ),
-  {
-    notes: "djjd"
-  }
-);
+storiesOf("Patterns|ProductCard", module)
+  .addParameters({ viewport: { defaultViewport: "iphone6" } })
+  .add(
+    "Card",
+    () => (
+      <ThemeProvider theme={theme}>
+        <ProductCard
+          title="Heading"
+          subtitle="subheading"
+          image="https://static-assets.glossier.com/production/spree/images/attachments/000/003/860/portrait_normal/01_ShopGrid.jpg"
+          secondImage="https://static-assets.glossier.com/production/spree/images/attachments/000/003/730/portrait_normal/bubblewrap.jpg"
+        />
+      </ThemeProvider>
+    ),
+    {
+      notes: "djjd"
+    },
+    { viewport: { defaultViewport: "iphonex" } }
+  );
